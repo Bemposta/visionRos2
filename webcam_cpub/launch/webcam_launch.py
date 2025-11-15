a54fc6f1e9d29314e436291fb08518e5736eeb9d
Sergio Bemposta <Bemposta@users.noreply.github.com>
2025-11-16 00:22:42 +0100

    lanzadores

---
+++ webcam_cpub/launch/webcam_launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+camera_publisher = Node(
+    package='webcam_cpub',
+    executable='publisher',
+    name='camera_publisher',
+    output='screen',
+    parameters=[
+        {"camera_id": -1},              #Si camera_id es <0, entonces usa el video como fuente de imagenes.
+        {"topic": "/camera/image"},     #El topic del video comprimido es topic+"/compressed"
+        {"fps": 2.0},                   #Tiene que ser un double. No puede ser integer.
+        {"video": "/home/mixi/ros2_ws/Tokyo_640.mp4"}
+    ]
+)
+
+camera_viewer = Node(
+    package='webcam_cpub',
+    executable='subscriber',
+    name='camera_viewer',
+    output='screen'
+)
+
+def generate_launch_description():
+    launchDescription = LaunchDescription([
+        camera_publisher,
+        camera_viewer
+    ])
+    return launchDescription
